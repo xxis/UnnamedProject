0a54b89a5adcfd995c9932e034dcbb3f71302ee0
David <dedmeades1@gmail.com>
2016-08-06 21:47:30 +0930

    Added ClientService with pouchdb

--- app/app.ts
+++ app/app.ts
@@ -2,10 +2,11 @@ import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
-
+import {ClientService} from './providers/clientservice/clientservice'
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
+  , providers: [ClientService]
 })
 export class MyApp {
 
@@ -22,4 +23,4 @@ export class MyApp {
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp, [ClientService]);
--- app/providers/clientservice/clientservice.ts
+++ app/providers/clientservice/clientservice.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Storage, SqlStorage } from 'ionic-angular';
 import 'rxjs/add/operator/map';
+import * as PouchDB from 'pouchdb';
 
 /*
   Generated class for the Clientservice provider.
@@ -9,9 +11,100 @@ import 'rxjs/add/operator/map';
   for more info on providers and Angular 2 DI.
 */
 @Injectable()
-export class Clientservice {
+export class ClientService {
+  data: any;
+  db: any;
+  remote: any;
+  username: any;
+  password: any;
 
-  constructor(private http: Http) {}
+  constructor(private http: Http) {
+
+    this.db = new PouchDB('cloudo');
+    this.username = 'surnialleareauslowitharm';
+    this.password = '71302fd0fb26de84161a65b8c68cfed38bf19bbb';
+    this.remote = 'https://dedmeades1.cloudant.com/clients/_all_docs';
+
+    let options = {
+      live: true,
+      retry: true,
+      continuous: true,
+      auth: {
+        username: this.username,
+        password: this.password
+      }
+    };
+
+    this.db.sync(this.remote, options);
+  }
+
+  getClients() {
+
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+      this.db.allDocs({
+        include_docs: true
+      }).then((result) => {
+        this.data = [];
+        let docs = result.rows.map((row) => {
+          this.data.push(row.doc);
+        });
+        resolve(this.data);
+        this.db.changes({ live: true, since: 'now', include_docs: true }).on('change', (change) => {
+          this.handleChange(change);
+        });
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
+  }
+
+  createClient(client) {
+    this.db.post(client);
+  }
+
+  updateClient(client) {
+    this.db.put(client).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  deleteClient(client) {
+    this.db.remove(client).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  handleChange(change) {
+
+    let changedDoc = null;
+    let changedIndex = null;
+
+    this.data.forEach((doc, index) => {
+      if (doc._id === change.id) {
+        changedDoc = doc;
+        changedIndex = index;
+      }
+    });
+
+    //A document was deleted
+    if (change.deleted) {
+      this.data.splice(changedIndex, 1);
+    }
+    else {
+      //A document was updated
+      if (changedDoc) {
+        this.data[changedIndex] = change.doc;
+      }
+      //A document was added
+      else {
+        this.data.push(change.doc);
+      }
+    }
+  }
 
 }
 
